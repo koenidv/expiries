dccd0ebd3265222d90bdac43c60d422bb5b48268
Florian König <koenidv@gmail.com>
2022-10-05 15:19:50 +0200

    Remove options menu

--- app/src/main/java/de/koenidv/expiries/MainActivity.kt
+++ app/src/main/java/de/koenidv/expiries/MainActivity.kt
@@ -2,8 +2,6 @@ package de.koenidv.expiries
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -61,22 +59,6 @@ class MainActivity : AppCompatActivity() {
                 Log.d("Database", db.articleDao().getAllSorted().toString())
             }
         }.show(supportFragmentManager, "editor")
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
