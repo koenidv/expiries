c46dca8633c3cf9c7311c1ce491e74366cc7ab8c
Florian König <koenidv@gmail.com>
2022-10-06 12:39:02 +0200

    Refactor ScannerSheet

--- app/src/main/java/de/koenidv/expiries/ScannerSheet.kt
+++ app/src/main/java/de/koenidv/expiries/ScannerSheet.kt
@@ -7,22 +7,20 @@ import android.util.Size
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.google.android.material.button.MaterialButton
 import com.google.mlkit.vision.barcode.BarcodeScanner
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import de.koenidv.expiries.databinding.SheetScannerBinding
 import permissions.dispatcher.NeedsPermission
 import permissions.dispatcher.RuntimePermissions
 import java.util.concurrent.ExecutorService
@@ -31,123 +29,134 @@ import java.util.concurrent.Executors
 @RuntimePermissions
 class ScannerSheet(val scannedCallback: (String?) -> Unit) : BottomSheetDialogFragment() {
 
+    private var _binding: SheetScannerBinding? = null
+    private val binding get() = _binding!!
     private lateinit var cameraExecutor: ExecutorService
-    private lateinit var preview: PreviewView
-    private lateinit var loadingProgress: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.sheet_camera, container, false)
+    ): View {
+        _binding = SheetScannerBinding.inflate(inflater, container, false)
         setStyle(DialogFragment.STYLE_NORMAL, R.style.BottomSheetDialogTheme)
 
-        preview = view.findViewById(R.id.camera_preview)
-        loadingProgress = view.findViewById(R.id.loading_progress)
         cameraExecutor = Executors.newSingleThreadExecutor()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         startCameraWithPermissionCheck()
 
-        view.findViewById<MaterialButton>(R.id.addManuallyButton).setOnClickListener {
+        binding.addManuallyButton.setOnClickListener {
             scannedCallback(null)
             dismiss()
         }
 
-        view.findViewById<MaterialButton>(R.id.cancelButton).setOnClickListener {
+        binding.cancelButton.setOnClickListener {
             dismiss()
         }
-
-        return view
-    }
-
-    override fun onDestroy() {
-        cameraExecutor.shutdown()
-        super.onDestroy()
     }
 
     @NeedsPermission(android.Manifest.permission.CAMERA)
     fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-
         cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-
-            val previewUseCase = Preview.Builder()
-                .setTargetResolution(Size(preview.width, preview.height))
-                .build()
-                .also {
-                    it.setSurfaceProvider(preview.surfaceProvider)
-                }
-
-            val options = BarcodeScannerOptions.Builder()
-                .setBarcodeFormats(
-                    Barcode.FORMAT_EAN_13,
-                    Barcode.FORMAT_EAN_8
-                ).build()
-            val scanner = BarcodeScanning.getClient(options)
-            val network = NetworkUtils(requireContext())
-
-            var lastResult: String? = null
-
-            val imageAnalyzer = ImageAnalysis.Builder().build()
-                .also {
-                    it.setAnalyzer(cameraExecutor) { imageProxy ->
-                        processImageProxy(scanner, imageProxy) { result ->
-
-                            if (result.equals(lastResult)) return@processImageProxy
-
-                            lastResult = result
-
-                            if (result == null) return@processImageProxy
-
-                            network.cancelProductDataRequests()
-                            loadingProgress.visibility = View.VISIBLE
-
-                            network.getProductData(result) { productData ->
-                                if (productData == null) {
-                                    loadingProgress.visibility = View.GONE
-                                } else {
-                                    scannedCallback(productData)
-                                    dismiss()
-                                }
-                            }
-
-                        }
-                    }
-                }
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, previewUseCase, imageAnalyzer
-                )
-            } catch (exc: Exception) {
-                Log.e("Camera", "Use case binding failed", exc)
-            }
-
+            bindUseCases(cameraProviderFuture.get())
         }, ContextCompat.getMainExecutor(requireContext()))
     }
 
+    private fun bindUseCases(camera: ProcessCameraProvider) {
+        try {
+            camera.bindToLifecycle(
+                this,
+                CameraSelector.DEFAULT_BACK_CAMERA,
+                getPreviewUseCase(),
+                getImageAnalyzer()
+            )
+        } catch (exc: Exception) {
+            Log.e("Camera", "Use case binding failed", exc)
+        }
+    }
+
+    private fun getPreviewUseCase(): Preview {
+        val view = binding.cameraPreview
+        return Preview.Builder()
+            .setTargetResolution(Size(view.width, view.height))
+            .build()
+            .also {
+                it.setSurfaceProvider(view.surfaceProvider)
+            }
+    }
+
+    private var scanner: BarcodeScanner? = null
+    private fun getBarcodeScanner(): BarcodeScanner {
+        if (scanner != null) return scanner!!
+
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(
+                Barcode.FORMAT_EAN_13,
+                Barcode.FORMAT_EAN_8
+            ).build()
+        scanner = BarcodeScanning.getClient(options)
+        return scanner!!
+    }
+
+    private fun getImageAnalyzer(): ImageAnalysis {
+        val network = NetworkUtils(requireContext())
+        var lastResult: String? = null
+
+        val analysis = ImageAnalysis.Builder().build()
+        analysis.setAnalyzer(cameraExecutor) { image ->
+            processImage(image) { result ->
+
+                if (result.equals(lastResult)) return@processImage
+                lastResult = result
+                if (result == null) return@processImage
+
+                handleBarcodeScanned(result, network)
+            }
+        }
+
+        return analysis
+    }
+
     @SuppressLint("UnsafeOptInUsageError")
-    private fun processImageProxy(
-        barcodeScanner: BarcodeScanner,
-        imageProxy: ImageProxy,
-        callback: (String?) -> Unit
-    ) {
-        // Process the image and callback with the result
-        // Close the proxy after completion to allow for the next image
-        barcodeScanner.process(
-            InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
+    private fun processImage(image: ImageProxy, callback: (String?) -> Unit) {
+        getBarcodeScanner().process(
+            InputImage.fromMediaImage(image.image!!, image.imageInfo.rotationDegrees)
         )
             .addOnSuccessListener { barcodes ->
                 val barcode = barcodes.firstOrNull() ?: return@addOnSuccessListener
                 callback(barcode.rawValue)
             }
             .addOnFailureListener { callback(null) }
-            .addOnCompleteListener { imageProxy.close() }
+            .addOnCompleteListener { image.close() }
     }
+
+    private fun handleBarcodeScanned(barcode: String, network: NetworkUtils) {
+        network.cancelProductDataRequests()
+        binding.loadingProgress.visibility = View.VISIBLE
+
+        network.getProductData(barcode) { productData ->
+            if (productData == null) {
+                binding.loadingProgress.visibility = View.GONE
+            } else {
+                scannedCallback(productData)
+                dismiss()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+        _binding = null
+    }
+
 
     @Deprecated("Deprecated in Java")
     override fun onRequestPermissionsResult(
